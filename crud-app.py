ab029f4bf6c45ad69ee18d3bd419de32262a9809
voicehelper <111184695+voicehelper@users.noreply.github.com>
2026-03-03 17:22:57 +0800

    Update SHEET_INPUT to a specific Google Sheets URL

--- crud-app.py
+++ crud-app.py
@@ -18,7 +18,7 @@ gc = init_connection()
 # ==========================================
 # 2. 開啟指定的試算表與工作表
 # ==========================================
-SHEET_INPUT = "試算表網址"
+SHEET_INPUT = "https://docs.google.com/spreadsheets/d/1nVTNx_KZZ0B5QArCJ37XBC1jM6Zl459xvcNlXOHRZfE/edit?gid=0#gid=0"
 WORKSHEET_NAME = "工作表1"
 
 try:
